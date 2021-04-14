f707ac83e5541dacd31d806c891c124828be1489
Pedro <hello@perezv.ar>
2021-04-14 19:18:58 -0300

    Mejorar comandos de Docker

--- docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "Mounted $(pwd) in /mnt, use :l /mnt/file.hs to load $(pwd)/file.hs"
-docker run -it --rm -e "TERM=xterm-256color" --mount type=bind,src=$(pwd),dst=/mnt --name "haskell" haskell:8
